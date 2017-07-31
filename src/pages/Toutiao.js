ea7b28e2e529919460efd45a15e5a045a2f435d8
xlslucky <1621588443@qq.com>
2017-07-31 13:03:49 +0800

    头条

--- src/pages/Toutiao.js
+++ src/pages/Toutiao.js
@@ -12,30 +12,44 @@ export class Toutiao extends Component {
 
   request() {
     let { address } = this.state;
-    if (!address) {
-      alert('请输入地址！')
+    if (!/www.wukong.com\/question/g.test(address)) {
+      alert('请输入正确的地址！')
+      this.setState({address: ''})
       return false;
     }
     setInterval(()=>{
       fetch(address).then((e)=>{
-        console.log(e)
+        // console.log(e)
       })
     }, 1000)
   }
 
   render() {
     let { address } = this.state;
+    let btnStyle = {
+      height: '30px', 
+      border: '1px solid red', 
+      padding: '0 20px'
+    }
+    let textStyle = {
+      border: '1px solid red',
+      height: '28px',
+      marginRight: '10px'
+    }
     return (
       <div>
         <p>刷头条访问量</p>
         <input 
           type="text" 
+          style={textStyle}
           onChange={({target})=>{
             address = target.value
             this.setState({ address })
           }}
           value={address}/>
-        <button onClick={this.request.bind(this)}>btn</button>
+        <button 
+          style={btnStyle}
+          onClick={this.request.bind(this)}>btn</button>
       </div>
     )
   }
