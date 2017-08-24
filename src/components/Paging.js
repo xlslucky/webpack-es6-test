ed93187c369275bda1d9ad821ac51f8af6768ed4
xlslucky <1621588443@qq.com>
2017-08-24 14:24:58 +0800

    分页--没做

---
+++ src/components/Paging.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+export class Paging extends Component {
+
+  render() {
+    return (
+      <div className="paging">
+        我是分页
+      </div>
+    )
+  }
+}
